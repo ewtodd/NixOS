dbe7689dd4cdcaef24f3b36ec2be59d5a5c6eca7
Valarie Milton <157831739+vael3429@users.noreply.github.com>
2025-06-18 17:00:34 -0500

    LISE++ ***REMOVED***

--- hosts/v-desktop/home.nix
+++ hosts/v-desktop/home.nix
@@ -6,10 +6,13 @@
     imports = [ ../../common/home-manager/play-user.nix ];
   };
 
-  "v-work" = { config, pkgs, ... }: {
-    home.username = "v-work";
-    home.homeDirectory = "/home/v-work";
-    home.stateVersion = "25.05";
-    imports = [ ../../common/home-manager/work-user.nix ];
-  };
+  "v-work" = { config, pkgs, ... }:
+    let lisepp = pkgs.callPackage ../../packages/LISE++/default.nix { };
+    in {
+      home.username = "v-work";
+      home.homeDirectory = "/home/v-work";
+      home.stateVersion = "25.05";
+      imports = [ ../../common/home-manager/work-user.nix ];
+      home.packages = [ lisepp ];
+    };
 }
