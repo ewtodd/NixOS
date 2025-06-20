a4e789a7c563a6cbf006e04cfe6a759a0d467697
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-06-20 18:36:21 -0400

    Change laptop TLP settings so hopefully it is less slow...

--- modules/nixos/distrobox.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }: {
-  virtualisation.podman = {
-    enable = true;
-    dockerCompat = true;
-  };
-  environment.systemPackages = [ pkgs.distrobox ];
-  environment.shellAliases = { debian = "distrobox enter debian"; };
-}
