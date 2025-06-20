d8f5582266dcc163fbdc5b459e9f7234a16f0411
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-06-19 22:38:23 -0400

    Separate module for bluetooth.

--- modules/nixos/sway-de.nix
+++ modules/nixos/sway-de.nix
@@ -28,9 +28,7 @@
   };
 
   security.pam.services.swaylock-effects = { };
-  services.blueman.enable = true;
-  hardware.bluetooth.powerOnBoot = false;
-  services.udisks2.enable = true;
+    services.udisks2.enable = true;
   services.gvfs.enable = true;
 
   qt = {
