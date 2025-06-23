c17a3f8f918302ceb700d7cf2efe6d95b3f0cb0a
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-06-23 13:54:56 -0400

    Update nixvim to use fancy-cat. Will I ever get actual work done? Many people are wondering this.

--- common/home-manager/work-user.nix
+++ common/home-manager/work-user.nix
@@ -1,36 +1,10 @@
 { pkgs, ... }: {
+  imports = [ ./xdg.nix ];
   home.packages = [ pkgs.clang-tools pkgs.slack ];
 
   xdg.desktopEntries.steam = {
     name = "Steam";
     noDisplay = true;
-  };
-
-  xdg.desktopEntries = {
-    neovim = {
-      name = "Neovim";
-      genericName = "Text Edtior";
-      exec = "foot -e nvim %F";
-      terminal = false;
-      categories = [ "Utility" "TextEditor" ];
-      mimeType = [
-        "text/english"
-        "text/plain"
-        "text/x-makefile"
-        "text/x-c++hdr"
-        "text/x-c++src"
-        "text/x-chdr"
-        "text/x-csrc"
-        "text/x-java"
-        "text/x-moc"
-        "text/x-pascal"
-        "text/x-tcl"
-        "text/x-tex"
-        "application/x-shellscript"
-        "text/x-c"
-        "text/x-c++"
-      ];
-    };
   };
 
   programs.bash = {
