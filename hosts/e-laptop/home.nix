369fc8ac95fe92566e76320d9d244332bdf3d93e
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-06-24 09:38:58 -0400

    ***REMOVED*** Hyprland that ***REMOVED*** is witawawwy ***REMOVED*** and SwayFX is the king forever.

--- hosts/e-laptop/home.nix
+++ hosts/e-laptop/home.nix
@@ -7,9 +7,9 @@
       ../../common/home-manager/play-user.nix
       ../../modules/home-manager/waybar/waybar.nix
       ../../modules/home-manager/swaync/swaync.nix
-      ../../modules/home-manager/sway/sway-base.nix
-      ../../modules/home-manager/sway/sway-settings-play.nix
-      ../../modules/home-manager/sway/sway-laptop-settings.nix
+      ../../modules/home-manager/sway/sway.nix
+      ../../modules/home-manager/sway/play-user.nix
+      ../../modules/home-manager/sway/laptop-settings.nix
     ];
   };
 
@@ -21,9 +21,9 @@
       ../../common/home-manager/work-user.nix
       ../../modules/home-manager/waybar/waybar.nix
       ../../modules/home-manager/swaync/swaync.nix
-      ../../modules/home-manager/sway/sway-base.nix
-      ../../modules/home-manager/sway/sway-settings-work.nix
-      ../../modules/home-manager/sway/sway-laptop-settings.nix
+      ../../modules/home-manager/sway/sway.nix
+      ../../modules/home-manager/sway/work-user.nix
+      ../../modules/home-manager/sway/laptop-settings.nix
     ];
     programs.bash.shellAliases = {
       phone-home = "ssh e-work@ssh.ethanwtodd.com";
