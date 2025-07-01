f244699df233e8ca7c0a8c79b98a94077ea8a108
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-07-01 11:45:28 -0400

    Update laptop specific sway settings to include output configuration.

--- modules/home-manager/windowManagers/sway/settings/laptop.nix
+++ modules/home-manager/windowManagers/sway/settings/laptop.nix
@@ -1,5 +1,44 @@
 { config, pkgs, ... }: {
   wayland.windowManager.sway = {
-    config = { startup = [{ command = "blueman-applet"; }]; };
+    config = {
+      startup = [{ command = "blueman-applet"; }];
+      # Output configuration
+      output = {
+        "eDP-1" = {
+          resolution = "1920x1080";
+          position = "0,0";
+        };
+        "HDMI-A-2" = {
+          resolution = "1920x1080";
+          position = "-1920,0";
+        };
+      };
+
+      # Workspace output assignments
+      workspaceOutputAssign = [
+        {
+          workspace = "1";
+          output = "eDP-1";
+        }
+        {
+          workspace = "2";
+          output = "eDP-1";
+        }
+        {
+          workspace = "3";
+          output = "eDP-1";
+        }
+        {
+          workspace = "4";
+          output = "eDP-1";
+        }
+        {
+          workspace = "5";
+          output = "HDMI-A-2";
+        }
+      ];
+
+    };
+
   };
 }
