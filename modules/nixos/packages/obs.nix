2f9dbd9872bb938676f0f2035e512e08161eb07b
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-07-03 12:54:38 -0400

    chore

--- modules/nixos/packages/obs.nix
+++ modules/nixos/packages/obs.nix
@@ -30,7 +30,6 @@ in {
   programs.obs-studio = {
     enable = true;
     package = obs-studio;
-    #pkgs.qt6Packages.callPackage ../../../packages/obs-studio/default.nix { };
     plugins = with pkgs.obs-studio-plugins; [ obs-backgroundremoval ];
   };
 
