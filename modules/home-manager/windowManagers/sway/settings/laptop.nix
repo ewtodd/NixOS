ce0a9dfbce0e1a23d2d82411edec257afd9da8dc
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-07-03 11:44:05 -0400

    Long live the king. Fix icon padding in play account later.

--- modules/home-manager/windowManagers/sway/settings/laptop.nix
+++ modules/home-manager/windowManagers/sway/settings/laptop.nix
@@ -14,30 +14,6 @@
         };
       };
 
-      # Workspace output assignments
-      workspaceOutputAssign = [
-        {
-          workspace = "1";
-          output = "eDP-1";
-        }
-        {
-          workspace = "2";
-          output = "eDP-1";
-        }
-        {
-          workspace = "3";
-          output = "eDP-1";
-        }
-        {
-          workspace = "4";
-          output = "eDP-1";
-        }
-        {
-          workspace = "5";
-          output = "HDMI-A-2";
-        }
-      ];
-
     };
 
   };
