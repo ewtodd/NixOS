41fe50ed944442ad19ab914d4af9e96f21372028
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-07-09 16:03:41 -0400

    disable ssh again

--- hosts/e-desktop/base.nix
+++ hosts/e-desktop/base.nix
@@ -4,7 +4,7 @@
     ../../common/nixos/base.nix
     ../../modules/nixos/hardware/intel-graphics.nix
     ../../modules/nixos/desktops/desktopEnvironment.nix
-    ../../modules/nixos/services/ssh.nix
+    #../../modules/nixos/services/ssh.nix
     #../../modules/nixos/services/suspend-then-hibernate.nix
     ../../modules/nixos/packages/steam.nix
     ../../modules/nixos/packages/obs.nix
