47cf90094cba7433b1c735c009812369d4826c1c
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-07-10 11:38:58 -0400

    Re-add geant4 environment but now with flake.

---
+++ modules/dev-environments/geant4/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Geant4 environment with minimal ROOT and Python packages.";
+
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux"; # Change to your system
+      pkgs = nixpkgs.legacyPackages.${system};
+      geant4WithQt = pkgs.geant4.override { enableQt = true; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          root
+          liberation_ttf
+          geant4WithQt
+          cmake
+          libsForQt5.full
+          xorg.libXinerama
+          geant4.data.G4ABLA
+          geant4.data.G4INCL
+          geant4.data.G4PhotonEvaporation
+          geant4.data.G4RealSurface
+          geant4.data.G4EMLOW
+          geant4.data.G4NDL
+          geant4.data.G4PII
+          geant4.data.G4SAIDDATA
+          geant4.data.G4ENSDFSTATE
+          geant4.data.G4PARTICLEXS
+          geant4.data.G4TENDL
+          geant4.data.G4RadioactiveDecay
+          (python3.withPackages
+            (python-pkgs: with python-pkgs; [ matplotlib numpy mplhep ]))
+        ];
+      };
+    };
+}
