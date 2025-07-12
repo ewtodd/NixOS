1f58be79322cb25118852489ba8fc340c3915109
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-07-12 13:43:22 -0400

    xdg desktop

--- modules/nixos/desktops/cosmic/cosmic-de.nix
+++ modules/nixos/desktops/cosmic/cosmic-de.nix
@@ -1,11 +1,13 @@
-{ config, lib, pkgs, ... }:
- {
+{ config, lib, pkgs, ... }: {
   config = lib.mkIf (config.WindowManager == "cosmic") {
     services.desktopManager.cosmic = {
       enable = true;
       xwayland.enable = true;
     };
     services.displayManager.cosmic-greeter.enable = true;
-    environment.systemPackages = with pkgs; [ wl-clipboard ];
+    environment.systemPackages = with pkgs; [
+      wl-clipboard
+      xdg-desktop-portal-cosmic
+    ];
   };
 }
