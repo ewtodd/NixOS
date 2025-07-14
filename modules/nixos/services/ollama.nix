b09ad7ebb1f05ec677d23d660b1dd23b238d5728
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-07-14 10:15:48 -0400

    ROCm + Ollama support! AMD rocks!

---
+++ modules/nixos/services/ollama.nix
@@ -0,0 +1,9 @@
+{ pkgs, inputs, ... }:
+let unstable = inputs.unstable.legacyPackages.${pkgs.system};
+in {
+  services.ollama = {
+    enable = true;
+    acceleration = "rocm";
+    package = unstable.ollama-rocm;
+  };
+}
