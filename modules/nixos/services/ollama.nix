d5f332e42f8ef12345ec13c19889743d84488c9f
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-07-17 09:43:42 -0400

    enable tailscale/ssh, disable proton, need to figure out how to make them play well

--- modules/nixos/services/ollama.nix
+++ modules/nixos/services/ollama.nix
@@ -7,20 +7,5 @@ in {
     package = unstable.ollama-rocm;
   };
 
-  services.open-webui = {
-    enable = true;
-    host = "0.0.0.0"; # Allow access from any IP
-    port = 8080;
-  };
-  services.nginx = {
-    enable = true;
-    recommendedProxySettings = true;
-    recommendedTlsSettings = true;
-
-    virtualHosts."llm.ethanwtodd.com" = {
-      globalRedirect = "llm.ethanwtodd.com:8080";
-    };
-  };
-
-  networking.firewall.allowedTCPPorts = [ 8080 ];
+  
 }
