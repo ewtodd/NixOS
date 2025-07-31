64eaa58a2872e662946a478b3536b66bed2fc670
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-07-31 13:09:49 -0400

    take my sway startup settings out of global sway config

--- hosts/e-desktop/home.nix
+++ hosts/e-desktop/home.nix
@@ -10,6 +10,7 @@
         ../../modules/home-manager/zettelkasten/zk.nix
         ../../modules/home-manager/waybar/waybar.nix
         ../../modules/home-manager/windowManagers/windowManager.nix
+        ../../modules/home-manager/windowManagers/sway/settings/profile.nix
       ];
       home.packages = [ mtkclient ];
       programs.waybar.enable = true;
