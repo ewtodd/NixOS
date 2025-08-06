d5cd35874b35a7cfa04ceaccea351ac50f540e76
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-08-06 12:47:32 -0400

    add uproot back

--- hosts/e-desktop/base.nix
+++ hosts/e-desktop/base.nix
@@ -7,7 +7,7 @@
     ../../modules/nixos/services/ollama.nix
     #../../modules/nixos/services/ssh.nix
     #../../modules/nixos/services/tailscale.nix
-    ../../modules/nixos/services/protonvpn.nix
+    #../../modules/nixos/services/protonvpn.nix
     #../../modules/nixos/services/suspend-then-hibernate.nix
     ../../modules/nixos/packages/steam.nix
     ../../modules/nixos/packages/obs.nix
