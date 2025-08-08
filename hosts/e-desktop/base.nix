5ea68bdd67f71137f36cf363eaf4bbc827ce8988
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-08-08 15:34:33 -0400

    re enable ssh on e-desktop

--- hosts/e-desktop/base.nix
+++ hosts/e-desktop/base.nix
@@ -5,8 +5,8 @@
     ../../modules/nixos/hardware/amd-graphics.nix
     ../../modules/nixos/desktops/desktopEnvironment.nix
     ../../modules/nixos/services/ollama.nix
-    #../../modules/nixos/services/ssh.nix
-    #../../modules/nixos/services/tailscale.nix
+    ../../modules/nixos/services/ssh.nix
+    ../../modules/nixos/services/tailscale.nix
     #../../modules/nixos/services/protonvpn.nix
     #../../modules/nixos/services/suspend-then-hibernate.nix
     ../../modules/nixos/packages/steam.nix
