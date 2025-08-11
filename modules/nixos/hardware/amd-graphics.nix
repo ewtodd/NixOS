e988c5e4d1a46a6f9d05d393c0262db997267aa7
Valarie Milton <157831739+vael3429@users.noreply.github.com>
2025-08-11 08:16:35 -0500

    update kernel  but switch to stable mesa  to avoid  breaking 9060

--- modules/nixos/hardware/amd-graphics.nix
+++ modules/nixos/hardware/amd-graphics.nix
@@ -3,7 +3,7 @@ let unstable = inputs.unstable.legacyPackages.${pkgs.system};
 in {
   hardware.graphics = {
     enable = true;
-    package = unstable.mesa;
+    package = pkgs.mesa;
     enable32Bit = true;
     extraPackages = with unstable; [ vulkan-tools rocmPackages.clr.icd ];
 
