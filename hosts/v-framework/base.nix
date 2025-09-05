77f76357e9099f8c470f0f0175ddd2693bd78fcd
vael3429 <vmilto4@lsu.edu>
2025-09-04 23:54:36 -0500

    added new shell for analysis-env in v-work

--- hosts/v-framework/base.nix
+++ hosts/v-framework/base.nix
@@ -10,6 +10,9 @@
     ../../modules/nixos/packages/zoom.nix
     ../../modules/nixos/packages/starship.nix
     ../../modules/nixos/services/tailscale.nix
+    ../../modules/nixos/services/suspend-then-hibernate.nix
+    ../../modules/nixos/services/laptop-power.nix
+    ../../modules/nixos/services/fingerprint.nix
     ../../modules/nixos/packages/docker.nix
     ../../modules/nixos/packages/moonlight.nix
   ];
