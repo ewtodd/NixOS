67064d83a5fe5cf5c6d47ed3514711dc327ded28
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-09-08 23:18:50 -0400

    they finally fixed obs and v4l2loopback stuff

--- modules/nixos/packages/obs.nix
+++ modules/nixos/packages/obs.nix
@@ -1,36 +1,9 @@
-{ pkgs, inputs, lib, ... }:
-let
-  obs-studio =
-    (pkgs.obs-studio.override { browserSupport = false; }).overrideAttrs
-    (oldAttrs: {
-      version = "31.1.0-beta2";
-      src = pkgs.fetchFromGitHub {
-        owner = "obsproject";
-        repo = "obs-studio";
-        rev = "31.1.0-beta2";
-        hash = "sha256-Ck2Sw51aNzetFhmLvC600fAdN5BJ9CeCwzhcXxAqsR4=";
-      };
-      nativeBuildInputs = oldAttrs.nativeBuildInputs
-        ++ [ pkgs.extra-cmake-modules ];
-      patches = [
-        (pkgs.fetchurl {
-          url =
-            "https://raw.githubusercontent.com/NixOS/nixpkgs/refs/heads/nixos-25.05/pkgs/applications/video/obs-studio/fix-nix-plugin-path.patch";
-          hash = "sha256-RRbawNqIaR3UFvO3hUjMtT8je6QBbgdnk6wOfR3PHNo=";
-        })
-      ];
-      postUnpack = ''
-        mkdir -p $sourceRoot/plugins/obs-browser
-        touch $sourceRoot/plugins/obs-browser/CMakeLists.txt
-        mkdir -p $sourceRoot/plugins/obs-websocket
-        touch $sourceRoot/plugins/obs-websocket/CMakeLists.txt
-      '';
-    });
+{ pkgs, inputs, ... }:
+let unstable = inputs.unstable.legacyPackages.${pkgs.system};
 in {
   programs.obs-studio = {
     enable = true;
-    package = obs-studio;
+    package = unstable.obs-studio;
     plugins = with pkgs.obs-studio-plugins; [ obs-backgroundremoval ];
   };
-
 }
