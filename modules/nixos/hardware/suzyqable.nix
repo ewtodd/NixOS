1536081c7c5e1352a5fc68d4c1418797d4a51163
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-09-20 09:37:24 -0400

    chromium embedded controller debugging tools

---
+++ modules/nixos/hardware/suzyqable.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    # Core debugging tools
+    openocd
+    gdb
+    minicom
+    screen
+
+    usbutils
+    libusb1
+
+    flashrom
+  ];
+  services.udev.extraRules = ''
+    # SuzyQable debug cable
+    SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="5014", MODE="0666", GROUP="plugdev"
+    SUBSYSTEM=="tty", ATTRS{idVendor}=="18d1", ATTRS{idProduct}=="5014", MODE="0666", GROUP="dialout"
+  '';
+}
