93917baff087f8daa1543476dd3fd74686781d5b
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-09-20 09:41:18 -0400

    who even knows what changes

--- modules/home-manager/windowManagers/sway/services/swayidle.nix
+++ modules/home-manager/windowManagers/sway/services/swayidle.nix
@@ -7,11 +7,11 @@
     timeouts = [
       {
         timeout = 600;
-        command = "conditional-lock";
+        command = "${pkgs.swaylock-effects}/bin/swaylock";
       }
       {
         timeout = 660;
-        command = "conditional-suspend";
+        command = "${pkgs.systemd}/bin/systemctl suspend";
       }
     ];
 
@@ -27,44 +27,4 @@
       }
     ];
   };
-  home.packages = with pkgs; [
-    (writeShellScriptBin "audio-check" ''
-      #!/bin/bash
-
-      # Check if audio is playing using pipewire/pulseaudio
-      if command -v pw-cli >/dev/null 2>&1; then
-        # PipeWire check
-        audio_playing=$(pw-cli info all | grep -E "state.*running" | grep -v "suspended")
-      else
-        # PulseAudio fallback
-        audio_playing=$(pactl list sink-inputs | grep -E "State: RUNNING")
-      fi
-
-      if [[ -n "$audio_playing" ]]; then
-        echo "audio_playing"
-      else
-        echo "audio_idle"
-      fi
-    '')
-
-    (writeShellScriptBin "conditional-lock" ''
-      #!/bin/bash
-
-      # Check audio status
-      audio_status=$(audio-check)
-
-      if [[ "$audio_status" == "audio_idle" ]]; then
-        ${pkgs.swaylock-effects}/bin/swaylock
-      fi
-    '')
-
-    (writeShellScriptBin "conditional-suspend" ''
-      #!/bin/bash
-      audio_status=$(audio-check)
-
-      if [[ "$audio_status" == "audio_idle" ]]; then
-        ${pkgs.systemd}/bin/systemctl suspend
-      fi
-    '')
-  ];
 }
