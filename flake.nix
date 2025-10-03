6b51c6be645d223d04498c32213c13e77009d1bc
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-10-02 23:15:47 -0400

    greetd hyprlock is a no go, it has no support as currently implemented for multiple users

--- flake.nix
+++ flake.nix
@@ -16,10 +16,6 @@
       url = "github:cynicsketch/nix-mineral";
       flake = false;
     };
-    hyprlock-greetd = {
-      url = "github:PaideiaDilemma/hyprlock?ref=greetdLogin";
-      inputs.nixpkgs.follows = "unstable";
-    };
   };
 
   outputs = inputs@{ self, nixpkgs, unstable, ... }: {
--- modules/home-manager/windowManagers/hypr/services/hyprlock.nix
+++ modules/home-manager/windowManagers/hypr/services/hyprlock.nix
@@ -1,8 +1,4 @@
-{ inputs, pkgs, ... }:
-
+{ ... }:
 {
-  programs.hyprlock = {
-    enable = true;
-    package = inputs.hyprlock-greetd.packages."${pkgs.system}".hyprlock;
-  };
+  programs.hyprlock = { enable = true; };
 }
