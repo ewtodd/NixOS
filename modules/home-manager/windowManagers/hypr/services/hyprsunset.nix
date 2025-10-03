66d121ff48478589c9851a02ab8b97bf6bc80796
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-10-02 21:28:46 -0400

    Do things the old fashioned way, because the design of hyprsunset home-manager module is wrong in 25.05.

--- modules/home-manager/windowManagers/hypr/services/hyprsunset.nix
+++ modules/home-manager/windowManagers/hypr/services/hyprsunset.nix
@@ -1,18 +1,18 @@
 { inputs, ... }:
 let unstable = import inputs.unstable { system = "x86_64-linux"; };
 in {
-  services.hyprsunset = {
-    enable = true;
-    package = unstable.hypridle;
-    transitions = {
-      sunrise = {
-        calendar = "*-*-* 06:00:00";
-        requests = [[ "temperature" "6500" ]];
-      };
-      sunset = {
-        calendar = "*-*-* 19:00:00";
-        requests = [[ "temperature" "4750" ]];
-      };
-    };
-  };
+  wayland.windowManager.hyprland.settings.exec-once = [ "hyprsunset" ];
+  home.packages = with unstable; [ hyprsunset ];
+  home.file.".config/hypr/hyprsunset.conf".text = ''
+
+    profile {
+        time = 6:00
+        identity = true
+    }
+
+    profile {
+        time = 21:00
+        temperature = 4750 
+    }
+  '';
 }
