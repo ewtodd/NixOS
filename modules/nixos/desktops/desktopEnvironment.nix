1e769fa34c3ca4a966b1582ba0b59f64aa71117e
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-10-22 23:14:46 -0400

    get rid of useless wlr git

--- modules/nixos/desktops/desktopEnvironment.nix
+++ modules/nixos/desktops/desktopEnvironment.nix
@@ -5,19 +5,6 @@ let
   nix-colors-lib = inputs.nix-colors.lib.contrib { inherit pkgs; };
 in {
 
-  nixpkgs.overlays = [
-    (self: super: {
-      xdg-desktop-portal-wlr = super.xdg-desktop-portal-wlr.overrideAttrs {
-        src = self.fetchFromGitHub {
-          owner = "emersion";
-          repo = "xdg-desktop-portal-wlr";
-          rev = "a08b8516740e325ea14a738652693856cfffa011";
-          sha256 = "sha256-0zIRCA1z7df9IU3PouwEJBHiETaJaYj9lwpmE1B1fOU=";
-        };
-      };
-    })
-  ];
-
   imports = [ ./sway/sway-de.nix ];
   services.displayManager.sddm = {
     enable = true;
