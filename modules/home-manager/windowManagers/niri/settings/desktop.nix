9cf37b18b1c3d3b7c0fdd58049a69844217fbc31
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-11-20 23:24:39 -0500

    still need to convert profile.nix

--- modules/home-manager/windowManagers/niri/settings/desktop.nix
+++ modules/home-manager/windowManagers/niri/settings/desktop.nix
@@ -5,11 +5,19 @@
         position x=0 y=0
         mode "3440x1440@180.000000"
         variable-refresh-rate on-demand=true
+        layout {
+          default-column-width { proportion 0.33333; }
+          preset-column-widths {
+            proportion 0.33333
+            proportion 0.5
+            proportion 0.66667
+          }
+        }
     }
     output "HDMI-A-1" {
         transform "normal"
         position x=-1920 y=0
         mode "1920x1080@74.973000"
-    }
+      }
   '';
 }
