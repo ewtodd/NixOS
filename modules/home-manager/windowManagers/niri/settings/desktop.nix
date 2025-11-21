39f39dede12410e2744201693732a3128437d0f5
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-11-21 09:53:04 -0500

    reverse order of widths

--- modules/home-manager/windowManagers/niri/settings/desktop.nix
+++ modules/home-manager/windowManagers/niri/settings/desktop.nix
@@ -8,9 +8,9 @@
         layout {
           default-column-width { proportion 0.33333; }
           preset-column-widths {
-            proportion 0.33333
-            proportion 0.5
             proportion 0.66667
+            proportion 0.5
+            proportion 0.33333
           }
         }
     }
