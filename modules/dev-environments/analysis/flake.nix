f485eda2f0a5e3e437e0689b034805da926f448d
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-12-09 09:48:18 -0500

    fix: add shellHook to analysis-env in case it is still used by the boss.

--- modules/dev-environments/analysis/flake.nix
+++ modules/dev-environments/analysis/flake.nix
@@ -33,7 +33,15 @@
               h5py
             ]))
         ];
-        shellHook = "";
+        shellHook = ''
+          STDLIB_PATH="${pkgs.stdenv.cc.cc}/include/c++/${pkgs.stdenv.cc.cc.version}"
+          STDLIB_MACHINE_PATH="$STDLIB_PATH/x86_64-unknown-linux-gnu"
+          export CPLUS_INCLUDE_PATH="$STDLIB_PATH:$STDLIB_MACHINE_PATH:$PWD/include:$(root-config --incdir):$CPLUS_INCLUDE_PATH"
+          export ROOT_INCLUDE_PATH="$PWD/include:$(root-config --incdir)"
+          export LD_LIBRARY_PATH="$PWD/lib:$LD_LIBRARY_PATH"
+
+          echo "C++ stdlib: $STDLIB_PATH"
+        '';
       };
     };
 }
