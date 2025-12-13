ef09e3105e79e23f57dca4348d73536a803817d0
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-12-13 14:35:41 -0500

    feat: add DankPomodoroTimer plugin. fix: clean up json

--- modules/home-manager/dms/colors.nix
+++ modules/home-manager/dms/colors.nix
@@ -1,51 +1,54 @@
-{ config, ... }:
-let colors = config.colorScheme.palette;
+{ config, pkgs, ... }:
+let
+  colors = config.colorScheme.palette;
+  jsonFormat = pkgs.formats.json { };
 in {
-  xdg.configFile."DankMaterialShell/colors.json".text = builtins.toJSON {
-    dark = {
-      name = "Eris Dark";
-      primary = "#${colors.base0E}";
-      primaryText = "#${colors.base00}";
-      primaryContainer = "#${colors.base0D}";
-      secondary = "#${colors.base08}";
-      surface = "#${colors.base00}";
-      surfaceText = "#${colors.base05}";
-      surfaceVariant = "#${colors.base01}";
-      surfaceVariantText = "#${colors.base05}";
-      surfaceTint = "#${colors.base0E}";
-      background = "#${colors.base00}";
-      backgroundText = "#${colors.base05}";
-      outline = "#${colors.base04}";
-      surfaceContainer = "#${colors.base01}";
-      surfaceContainerHigh = "#${colors.base02}";
-      surfaceContainerHighest = "#${colors.base03}";
-      error = "#${colors.base08}";
-      warning = "#${colors.base0A}";
-      info = "#${colors.base0C}";
-      matugen_type = "scheme-fidelity";
-    };
-    light = {
-      name = "Eris Dark";
-      primary = "#${colors.base0E}";
-      primaryText = "#${colors.base00}";
-      primaryContainer = "#${colors.base0D}";
-      secondary = "#${colors.base08}";
-      surface = "#${colors.base00}";
-      surfaceText = "#${colors.base05}";
-      surfaceVariant = "#${colors.base01}";
-      surfaceVariantText = "#${colors.base05}";
-      surfaceTint = "#${colors.base0E}";
-      background = "#${colors.base00}";
-      backgroundText = "#${colors.base05}";
-      outline = "#${colors.base04}";
-      surfaceContainer = "#${colors.base01}";
-      surfaceContainerHigh = "#${colors.base02}";
-      surfaceContainerHighest = "#${colors.base03}";
-      error = "#${colors.base08}";
-      warning = "#${colors.base0A}";
-      info = "#${colors.base0C}";
-      matugen_type = "scheme-fidelity";
+  xdg.configFile."DankMaterialShell/colors.json" = {
+    source = jsonFormat.generate "colors.json" {
+      dark = {
+        name = "Eris Dark";
+        primary = "#${colors.base0E}";
+        primaryText = "#${colors.base00}";
+        primaryContainer = "#${colors.base0D}";
+        secondary = "#${colors.base08}";
+        surface = "#${colors.base00}";
+        surfaceText = "#${colors.base05}";
+        surfaceVariant = "#${colors.base01}";
+        surfaceVariantText = "#${colors.base05}";
+        surfaceTint = "#${colors.base0E}";
+        background = "#${colors.base00}";
+        backgroundText = "#${colors.base05}";
+        outline = "#${colors.base04}";
+        surfaceContainer = "#${colors.base01}";
+        surfaceContainerHigh = "#${colors.base02}";
+        surfaceContainerHighest = "#${colors.base03}";
+        error = "#${colors.base08}";
+        warning = "#${colors.base0A}";
+        info = "#${colors.base0C}";
+        matugen_type = "scheme-fidelity";
+      };
+      light = {
+        name = "Eris Dark";
+        primary = "#${colors.base0E}";
+        primaryText = "#${colors.base00}";
+        primaryContainer = "#${colors.base0D}";
+        secondary = "#${colors.base08}";
+        surface = "#${colors.base00}";
+        surfaceText = "#${colors.base05}";
+        surfaceVariant = "#${colors.base01}";
+        surfaceVariantText = "#${colors.base05}";
+        surfaceTint = "#${colors.base0E}";
+        background = "#${colors.base00}";
+        backgroundText = "#${colors.base05}";
+        outline = "#${colors.base04}";
+        surfaceContainer = "#${colors.base01}";
+        surfaceContainerHigh = "#${colors.base02}";
+        surfaceContainerHighest = "#${colors.base03}";
+        error = "#${colors.base08}";
+        warning = "#${colors.base0A}";
+        info = "#${colors.base0C}";
+        matugen_type = "scheme-fidelity";
+      };
     };
   };
 }
-
---
+++ modules/home-manager/dms/plugins.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+let
+  dmsPlugins = pkgs.fetchFromGitHub {
+    owner = "AvengeMedia";
+    repo = "dms-plugins";
+    rev = "8715ca35b61d7c6275a300fa4992e2b0490f70f7";
+    hash = "sha256-6ScuVcvSaXc35Sf1iwtCy8aM/pFID3+0G4NRMw8aBcM=";
+  };
+  jsonFormat = pkgs.formats.json { };
+in {
+
+  xdg.configFile."DankMaterialShell/plugin_settings.json" = {
+    source = jsonFormat.generate "plugin_settings.json" {
+      dankPomodoroTimer.enabled = true;
+    };
+  };
+
+  xdg.configFile."DankMaterialShell/plugins/" = {
+    source = dmsPlugins;
+    recursive = true;
+  };
+
+}
