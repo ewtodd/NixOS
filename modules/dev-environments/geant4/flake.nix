fbc8698d47867f7c0aeaa6d166d58ec01c1e6862
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-12-17 10:59:14 -0500

    fix: update geant4 flake to 25.11, had to remove libsForQt5.full from buildInputs. this did not break visualization.

--- modules/dev-environments/geant4/flake.nix
+++ modules/dev-environments/geant4/flake.nix
@@ -1,17 +1,21 @@
 {
   description = "Geant4 environment with minimal ROOT and Python packages.";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    utils.url = "github:ewtodd/Nuclear-Measurement-Utilities";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, utils }:
     let
-      system = "x86_64-linux"; # Change to your system
+      system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       geant4custom = pkgs.geant4.override {
         enableQt = true;
         enableOpenGLX11 = true;
         enableRaytracerX11 = true;
       };
+      nm-utils = utils.packages.${system}.default;
     in {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
@@ -19,7 +23,6 @@
           liberation_ttf
           geant4custom
           cmake
-          libsForQt5.full
           xorg.libXinerama
           geant4.data.G4ABLA
           geant4.data.G4INCL
@@ -53,6 +56,22 @@
 
           # XWayland fallback for problematic apps
           export DISPLAY=:0
+          echo "ROOT version: $(root-config --version)"
+          echo "Nuclear-Measurement-Utilities: ${nm-utils}"
+
+          STDLIB_PATH="${pkgs.stdenv.cc.cc}/include/c++/${pkgs.stdenv.cc.cc.version}"
+          STDLIB_MACHINE_PATH="$STDLIB_PATH/x86_64-unknown-linux-gnu"
+
+          ROOT_INC="$(root-config --incdir)"
+          # Local first, then remote, then others
+          export CPLUS_INCLUDE_PATH="$PWD/include:$STDLIB_PATH:$STDLIB_MACHINE_PATH:${nm-utils}/include:$ROOT_INC''${CPLUS_INCLUDE_PATH:+:$CPLUS_INCLUDE_PATH}"
+
+          export PKG_CONFIG_PATH="${nm-utils}/lib/pkgconfig:$PKG_CONFIG_PATH"
+
+          export ROOT_INCLUDE_PATH="$PWD/include:${nm-utils}/include''${ROOT_INCLUDE_PATH:+:$ROOT_INCLUDE_PATH}"
+          # Local lib first means linker will use it preferentially
+          export LD_LIBRARY_PATH="$PWD/lib:${nm-utils}/lib''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+
         '';
       };
     };
