8f48aa3325f9a49e5b8eb79d4f4f865b45ab5cbf
Valarie Milton <157831739+vael3429@users.noreply.github.com>
2025-12-28 19:26:00 -0600

    feat: configure v-desktop monitors properly. turns out they sent one with a 144Hz refresh rate instead of 75Hz!

--- modules/home-manager/niri/settings/desktop.nix
+++ modules/home-manager/niri/settings/desktop.nix
@@ -19,7 +19,12 @@
     output "HDMI-A-1" {
         transform "normal"
         position x=-1920 y=0
-        mode "1920x1080@74.973000"
+        mode "1920x1080@120.002"
+    }
+    output "HDMI-A-5" {
+        transform "normal"
+        position x=0 y=0
+        mode "1920x1080@75.000"
     }
   '';
 }
