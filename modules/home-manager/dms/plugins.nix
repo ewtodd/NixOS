f90d12c62c2607848c7c7618db30fc8a3fbb110b
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-01-04 14:01:57 -0500

    feat: update dms settings for revised home-manager module

--- modules/home-manager/dms/plugins.nix
+++ modules/home-manager/dms/plugins.nix
@@ -6,19 +6,16 @@ let
     rev = "8715ca35b61d7c6275a300fa4992e2b0490f70f7";
     hash = "sha256-6ScuVcvSaXc35Sf1iwtCy8aM/pFID3+0G4NRMw8aBcM=";
   };
-  jsonFormat = pkgs.formats.json { };
 in
 {
-
-  xdg.configFile."DankMaterialShell/plugin_settings.json" = {
-    source = jsonFormat.generate "plugin_settings.json" {
-      dankPomodoroTimer.enabled = true;
+  programs.dank-material-shell = {
+    managePluginSettings = true;
+    plugins = {
+      dankPomodoroTimer = {
+        enable = true;
+        src = "${dmsPlugins}/DankPomodoroTimer";
+      };
     };
-  };
-
-  xdg.configFile."DankMaterialShell/plugins/" = {
-    source = dmsPlugins;
-    recursive = true;
   };
 
 }
