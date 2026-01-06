dc35a27c0625ddaf2984ec01525517291e75472a
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-01-05 21:14:25 -0500

    delete: xserver lines from when gdm was around

--- modules/nixos/desktopEnvironment/desktopEnvironment.nix
+++ modules/nixos/desktopEnvironment/desktopEnvironment.nix
@@ -1,11 +1,6 @@
-{ pkgs, ... }:
+{ ... }:
 {
-
   imports = [ ./niri/niri-de.nix ];
-  services.xserver = {
-    displayManager.startx.enable = false;
-    excludePackages = with pkgs; [ xterm ];
-  };
   programs.dank-material-shell.greeter = {
     enable = true;
     compositor.name = "niri";
