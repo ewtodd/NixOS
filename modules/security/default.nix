a23940a1009fc870b1e1fbbc4dc62db0d5d7e49a
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-01-25 17:30:37 -0500

    fix: remove all network sysctl settings from harden module since something in there completely broke adding new wifi...

--- modules/security/default.nix
+++ modules/security/default.nix
@@ -86,31 +86,6 @@
       "kernel.kexec_load_disabled" = 1;
       "kernel.sysrq" = 4;
 
-      "net.ipv4.tcp_syncookies" = 1;
-      "net.ipv4.tcp_rfc1337" = 1;
-      "net.ipv4.conf.default.rp_filter" = 1;
-      "net.ipv4.conf.all.rp_filter" = 1;
-
-      "net.ipv4.conf.all.accept_redirects" = 0;
-      "net.ipv4.conf.default.accept_redirects" = 0;
-      "net.ipv4.conf.all.secure_redirects" = 0;
-      "net.ipv4.conf.default.secure_redirects" = 0;
-      "net.ipv6.conf.all.accept_redirects" = 0;
-      "net.ipv6.conf.default.accept_redirects" = 0;
-      "net.ipv4.conf.all.send_redirects" = 0;
-      "net.ipv4.conf.default.send_redirects" = 0;
-
-      "net.ipv4.conf.all.forwarding" = 0;
-      "net.ipv4.conf.default.accept_source_route" = 0;
-      "net.ipv4.conf.all.accept_source_route" = 0;
-      "net.ipv6.conf.all.accept_source_route" = 0;
-      "net.ipv6.conf.default.accept_source_route" = 0;
-      "net.ipv6.conf.all.forwarding" = 0;
-      "net.ipv6.conf.all.accept_ra" = 0;
-      "net.ipv6.conf.default.accept_ra" = 0;
-
-      "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
-
       "fs.protected_symlinks" = 1;
       "fs.protected_hardlinks" = 1;
       "fs.protected_fifos" = 2;
@@ -118,10 +93,6 @@
 
       "kernel.randomize_va_space" = 2;
       "kernel.exec-shield" = 1;
-
-      "net.ipv4.tcp_fastopen" = 3;
-      "net.ipv4.tcp_congestion_control" = "bbr";
-      "net.core.default_qdisc" = "cake";
     };
 
     systemd.coredump.enable = false;
@@ -135,9 +106,7 @@
     networking = {
       networkmanager = {
         enable = true;
-        wifi.scanRandMacAddress = true;
         wifi.macAddress = "random";
-        plugins = [ ];
       };
 
       firewall = {
