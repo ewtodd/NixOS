281c654f8bc1aa8cc1d8e6538de967327b46a50f
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-01-26 16:18:46 -0500

    fix: ai crap. change: openconnect shortcut to reference nix store path

--- home-manager/packages/ai/default.nix
+++ home-manager/packages/ai/default.nix
@@ -14,6 +14,9 @@ in
       enable = true;
       package = unstable.ollama;
       acceleration = "rocm";
+      environmentVariables = {
+        OLLAMA_CONTEXT_LENGTH = "16384";
+      };
     };
 
     programs.opencode = {
@@ -23,18 +26,28 @@ in
         provider = {
           ollama = {
             name = "Ollama (local)";
+            npm = "@ai-sdk/openai-compatible";
             options = {
-              baseURL = "http://127.0.0.1:11434/v1";
+              baseURL = "http://127.0.0.1:11434";
             };
             models = {
               "llama3.2" = {
                 name = "Llama 3.2";
+                options = {
+                  num_ctx = 16384;
+                };
               };
               "gpt-oss:20b" = {
                 name = "GPT-oss";
+                options = {
+                  num_ctx = 16384;
+                };
               };
               "qwen3-coder:latest" = {
                 name = "Qwen3-coder";
+                options = {
+                  num_ctx = 16384;
+                };
               };
             };
           };
