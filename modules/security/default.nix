708c82972cb0b9419cee813d3eb52055eba3da8b
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-01-26 21:25:23 -0500

    remove everything from hardening that i don't understand exactly what it does. will add back bit by bit later. chore: update flake.lock change: e-devices: xanmod_stable->xanmod_latest

--- modules/security/default.nix
+++ modules/security/default.nix
@@ -32,16 +32,6 @@
       ];
     };
 
-    boot.kernelParams = [
-      "slab_nomerge"
-      "page_alloc.shuffle=1"
-      "randomize_kstack_offset=on"
-      "vsyscall=none"
-      "debugfs=off"
-      "oops=panic"
-      "lockdown=confidentiality"
-    ];
-
     boot.blacklistedKernelModules = [
       "dccp"
       "sctp"
@@ -69,22 +59,6 @@
       "hfsplus"
       "udf"
     ];
-
-    boot.kernel.sysctl = {
-      "kernel.kptr_restrict" = 2;
-      "kernel.dmesg_restrict" = 1;
-      "vm.unprivileged_userfaultfd" = 0;
-      "kernel.kexec_load_disabled" = 1;
-      "kernel.sysrq" = 4;
-
-      "fs.protected_symlinks" = 1;
-      "fs.protected_hardlinks" = 1;
-      "fs.protected_fifos" = 2;
-      "fs.protected_regular" = 2;
-
-      "kernel.randomize_va_space" = 2;
-      "kernel.exec-shield" = 1;
-    };
 
     systemd.coredump.enable = false;
 
