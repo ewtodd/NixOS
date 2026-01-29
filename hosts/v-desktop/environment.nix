82904e722f1e1f4a42c15aaf1e9c62afefdd25d6
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-01-29 13:39:07 -0500

    change: switch to lqx kernel. feat: add um library bookmark in firefox. fix: add some local ai, delete terrible pstate kernel param for e-laptop.

--- hosts/v-desktop/environment.nix
+++ hosts/v-desktop/environment.nix
@@ -4,7 +4,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_lqx;
 
   boot.kernelParams = [
     "quiet"
--- hosts/v-laptop/environment.nix
+++ hosts/v-laptop/environment.nix
@@ -3,7 +3,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_lqx;
   boot.kernelParams = [
     "quiet"
     "splash"
