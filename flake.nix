6c7d95041ff63c3142c214ae301ab196195a6f24
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-02-18 14:55:46 -0500

    cachy kernel for e

--- flake.nix
+++ flake.nix
@@ -55,6 +55,7 @@
       url = "github:nix-community/lanzaboote/v1.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
   };
 
   outputs =
