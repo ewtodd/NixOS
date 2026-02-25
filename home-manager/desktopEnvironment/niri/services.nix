fc647e80532ede4a4ec231dfb35116cdef2c82b9
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-02-25 14:06:17 -0500

    feat: niri-sidebar!

--- home-manager/desktopEnvironment/niri/services.nix
+++ home-manager/desktopEnvironment/niri/services.nix
@@ -1,4 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, osConfig, ... }:
+let
+  e = osConfig.systemOptions.owner.e.enable;
+  isLaptop = osConfig.systemOptions.deviceType.laptop.enable;
+  margins = 10;
+  gap = 10;
+  nWindows = 3;
+  height =
+    if isLaptop then
+      builtins.floor ((1504 - 2 * margins - nWindows * gap) / (nWindows * 1.35))
+    else
+      (
+        if e then
+          builtins.floor ((1440 - 2 * margins - nWindows * gap) / nWindows)
+        else
+          builtins.floor ((1080 - 2 * margins - nWindows * gap) / nWindows)
+      );
+in
 {
   config = {
     services.udiskie = {
@@ -11,5 +28,28 @@
       };
     };
     services.gnome-keyring.enable = true;
+    programs.niri-sidebar = {
+      enable = true;
+      settings = {
+        geometry = {
+          width = 500;
+          height = height;
+          gap = gap;
+        };
+        margins = {
+          top = margins;
+          right = margins;
+          left = margins;
+          bottom = margins;
+        };
+        interaction = {
+          position = if e then "left" else "right";
+          peek = 15;
+          sticky = true;
+          auto_focus_layer = true;
+        };
+      };
+      systemd.enable = true;
+    };
   };
 }
