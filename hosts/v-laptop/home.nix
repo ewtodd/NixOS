57c4ebc32fb87bc5af536ea92bc85aa51cd82aec
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-03-10 14:12:49 -0400

    fix: remove unused imports, use old eris for v

--- hosts/v-laptop/home.nix
+++ hosts/v-laptop/home.nix
@@ -1,6 +1,12 @@
 {
   "v-play" =
-    { inputs, pkgs, ... }:
+    { pkgs, ... }:
+    let
+      eris = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/tinted-theming/base16-schemes/refs/heads/main/eris.yaml";
+        hash = "sha256-p3E5ksta+ruhiTYhqpwnC/92LBR5dZ+BSd+ozVPncw0=";
+      };
+    in
     {
       home.username = "v-play";
       home.homeDirectory = "/home/v-play";
@@ -8,11 +14,11 @@
       imports = [
         ../../home-manager/profiles/play.nix
       ];
-      scheme = "${pkgs.base16-schemes}/share/themes/eris.yaml";
+      scheme = "${eris}";
     };
 
   "v-work" =
-    { pkgs, inputs, ... }:
+    { pkgs, ... }:
     {
       home.username = "v-work";
       home.homeDirectory = "/home/v-work";
