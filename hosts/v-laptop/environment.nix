f8a8c1d959ba1293713b82c3935d70bef83f96c9
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-03-17 12:26:10 -0400

    change v kernel to LTS

--- hosts/v-laptop/environment.nix
+++ hosts/v-laptop/environment.nix
@@ -2,7 +2,7 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages;
   boot.kernelParams = [
     "quiet"
     "splash"
