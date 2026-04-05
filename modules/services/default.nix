1ab992d333a2e80f3060ac3113d667e9bd69a677
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-04-05 15:53:25 -0400

    attempting orgmode+syncthing

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -36,51 +36,6 @@
         HibernateDelaySec = if (config.systemOptions.deviceType.laptop.enable) then "1800" else "3600";
       };
     })
-    (lib.mkIf (config.systemOptions.services.orgmode-sync.enable) (lib.mkMerge [
-      {
-        users.groups.org = { };
-
-        services.syncthing = {
-          enable = true;
-          group = "org";
-          dataDir = "/srv/syncthing";
-          openDefaultPorts = true;
-          overrideDevices = false;
-          overrideFolders = false;
-          settings = {
-            folders = {
-              "org" = {
-                path = "/srv/org";
-                versioning = {
-                  type = "simple";
-                  params.keep = "5";
-                };
-              };
-            };
-          };
-        };
-
-        systemd.tmpfiles.rules = [
-          "d /srv/org 2775 syncthing org - -"
-        ];
-      }
-      (lib.mkIf (config.systemOptions.owner.e.enable) {
-        users.users.e-play.extraGroups = [ "org" ];
-        users.users.e-work.extraGroups = [ "org" ];
-        systemd.tmpfiles.rules = [
-          "L /home/e-play/org - - - - /srv/org"
-          "L /home/e-work/org - - - - /srv/org"
-        ];
-      })
-      (lib.mkIf (!config.systemOptions.owner.e.enable) {
-        users.users.v-play.extraGroups = [ "org" ];
-        users.users.v-work.extraGroups = [ "org" ];
-        systemd.tmpfiles.rules = [
-          "L /home/v-play/org - - - - /srv/org"
-          "L /home/v-work/org - - - - /srv/org"
-        ];
-      })
-    ]))
     (lib.mkIf (config.systemOptions.services.tailscale.enable) {
       services.tailscale = {
         enable = true;
