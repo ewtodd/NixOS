a032ad2ba1a9e0a2544237a7140351d7e2d19fd7
Valarie Milton <157831739+vael3429@users.noreply.github.com>
2026-09-09 18:50:52 -0500

    fix monitors  for v

--- home-manager/desktopEnvironment/niri/settings/desktop.nix
+++ home-manager/desktopEnvironment/niri/settings/desktop.nix
@@ -50,7 +50,7 @@ let
       _args = [ "HDMI-A-5" ];
       transform = "normal";
       position._props = {
-        x = -1920;
+        x = 0;
         y = 0;
       };
       mode = "1920x1080@144.002";
@@ -59,7 +59,7 @@ let
       _args = [ "DP-4" ];
       transform = "normal";
       position._props = {
-        x = 0;
+        x = -1920;
         y = 0;
       };
       mode = "1920x1080@75.000";
